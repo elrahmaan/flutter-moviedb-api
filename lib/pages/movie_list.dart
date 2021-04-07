b557a6fde48b0f4bc435592767b2dd23150258d7
Abdul Rahman Saleh <abdulrahmansaleh152@gmail.com>
2021-04-07 15:30:12 +0700

    membuat file movie_list.dart

---
+++ lib/pages/movie_list.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MovieList extends StatefulWidget {
+  @override
+  _MovieListState createState() => _MovieListState();
+}
+
+class _MovieListState extends State<MovieList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
