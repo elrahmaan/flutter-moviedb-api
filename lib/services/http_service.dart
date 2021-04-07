389a35fafd25bc1a01a2fff3b94dfcc053a99bf9
Abdul Rahman Saleh <abdulrahmansaleh152@gmail.com>
2021-04-07 15:38:01 +0700

    membuat file helper http_service.dart

---
+++ lib/services/http_service.dart
@@ -0,0 +1,6 @@
+import 'package:http/http.dart' as http;
+
+class HttpService {
+  final String apiKey = 'ba10bb3b8df3add113d27c00d96facad';
+  final String baseUrl = 'https://api.themoviedb.org/3/movie/popular?api_key=';
+}
