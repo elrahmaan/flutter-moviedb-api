d325d7a4183e88c7e79713a057bc6ebfaf9cba68
Abdul Rahman Saleh <abdulrahmansaleh152@gmail.com>
2021-04-17 17:39:11 +0700

    update tampilan list item movie

--- lib/pages/movie_list.dart
+++ lib/pages/movie_list.dart
@@ -59,82 +59,176 @@ class _MovieListState extends State<MovieList> {
       ),
       body: Container(
         color: Colors.black87,
-        child: Container(
-          height: 220.0,
-          child: PageIndicatorContainer(
-            align: IndicatorAlign.bottom,
-            length: movies.take(5).length,
-            indicatorSpace: 8.0,
-            padding: const EdgeInsets.all(5.0),
-            indicatorColor: Colors.blueGrey[200],
-            indicatorSelectorColor: Colors.orangeAccent,
-            shape: IndicatorShape.circle(size: 6.0),
-            child: PageView.builder(
-              controller: pageController,
-              scrollDirection: Axis.horizontal,
-              itemCount: movies.take(5).length,
-              itemBuilder: (context, position) {
-                return InkWell(
-                  onTap: () {
-                    MaterialPageRoute route = MaterialPageRoute(
-                        builder: (_) => MovieDetail(movies[position]));
-                    Navigator.push(context, route);
-                  },
-                  child: Stack(
-                    children: <Widget>[
-                      Hero(
-                        tag: movies[position].id,
-                        child: Container(
-                            width: MediaQuery.of(context).size.width,
-                            height: 220.0,
-                            decoration: new BoxDecoration(
-                              shape: BoxShape.rectangle,
-                              image: new DecorationImage(
-                                  fit: BoxFit.cover,
-                                  image: NetworkImage(
-                                      "https://image.tmdb.org/t/p/original/" +
-                                          movies[position]
-                                              .posterPath
-                                              .toString())),
-                            )),
-                      ),
-                      Positioned(
-                          bottom: 30.0,
-                          child: Container(
-                            padding: EdgeInsets.only(left: 10.0, right: 10.0),
-                            width: 250.0,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text(
-                                  movies[position].title,
-                                  style: TextStyle(
-                                      height: 1.5,
-                                      color: Colors.white,
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 18.0),
-                                ),
-                                Container(
-                                  margin: EdgeInsets.only(top: 5),
-                                  child: Text(
-                                    'Rate: ' +
-                                        movies[position].voteAverage.toString(),
-                                    style: TextStyle(
-                                      color: Colors.yellow,
-                                      fontWeight: FontWeight.w700,
-                                      fontSize: 17,
+        child: Column(
+          children: [
+            //BANNER HERO IMAGE
+            Container(
+              height: 220.0,
+              child: PageIndicatorContainer(
+                align: IndicatorAlign.bottom,
+                length: movies.take(5).length,
+                indicatorSpace: 8.0,
+                padding: const EdgeInsets.all(5.0),
+                indicatorColor: Colors.blueGrey[200],
+                indicatorSelectorColor: Colors.orangeAccent,
+                shape: IndicatorShape.circle(size: 6.0),
+                child: PageView.builder(
+                  controller: pageController,
+                  scrollDirection: Axis.horizontal,
+                  itemCount: movies.take(5).length,
+                  itemBuilder: (context, position) {
+                    return InkWell(
+                      onTap: () {
+                        MaterialPageRoute route = MaterialPageRoute(
+                            builder: (_) => MovieDetail(movies[position]));
+                        Navigator.push(context, route);
+                      },
+                      child: Stack(
+                        children: <Widget>[
+                          Hero(
+                            tag: movies[position].id,
+                            child: Container(
+                                width: MediaQuery.of(context).size.width,
+                                height: 220.0,
+                                decoration: new BoxDecoration(
+                                  shape: BoxShape.rectangle,
+                                  image: new DecorationImage(
+                                      fit: BoxFit.cover,
+                                      image: NetworkImage(
+                                          "https://image.tmdb.org/t/p/original/" +
+                                              movies[position]
+                                                  .posterPath
+                                                  .toString())),
+                                )),
+                          ),
+                          Positioned(
+                              bottom: 30.0,
+                              child: Container(
+                                padding:
+                                    EdgeInsets.only(left: 10.0, right: 10.0),
+                                width: 250.0,
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      movies[position].title,
+                                      style: TextStyle(
+                                          height: 1.5,
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 18.0),
                                     ),
+                                    Container(
+                                      margin: EdgeInsets.only(top: 5),
+                                      child: Text(
+                                        'Rate: ' +
+                                            movies[position]
+                                                .voteAverage
+                                                .toString(),
+                                        style: TextStyle(
+                                          color: Colors.yellow,
+                                          fontWeight: FontWeight.w700,
+                                          fontSize: 17,
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              )),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ),
+
+            // POPULAR MOVIE
+            Container(
+              margin: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 10),
+                    child: Text(
+                      "Popular Movie",
+                      style: TextStyle(
+                        color: Colors.white,
+                        // color: Color(0xFFf4C10F),
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    height: 230,
+                    margin: EdgeInsets.only(top: 10),
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: movies.length,
+                      itemBuilder: (context, position) {
+                        return InkWell(
+                          onTap: () {
+                            MaterialPageRoute route = MaterialPageRoute(
+                                builder: (_) => MovieDetail(movies[position]));
+                            Navigator.push(context, route);
+                          },
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                height: 180,
+                                width: 140,
+                                margin: EdgeInsets.only(right: 15),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  image: DecorationImage(
+                                    fit: BoxFit.fill,
+                                    image: NetworkImage(
+                                        "https://image.tmdb.org/t/p/original/" +
+                                            movies[position]
+                                                .posterPath
+                                                .toString()),
                                   ),
                                 ),
-                              ],
-                            ),
-                          )),
-                    ],
+                              ),
+                              Container(
+                                width: 140,
+                                child: Text(
+                                  movies[position].title,
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.w500,
+                                    fontSize: 17,
+                                  ),
+                                  maxLines: 1,
+                                  overflow: TextOverflow.clip,
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(top: 5),
+                                child: Text(
+                                  'Rate: ' +
+                                      movies[position].voteAverage.toString(),
+                                  style: TextStyle(
+                                    color: Colors.yellow,
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 15,
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        );
+                      },
+                    ),
                   ),
-                );
-              },
+                ],
+              ),
             ),
-          ),
+          ],
         ),
       ),
     );
