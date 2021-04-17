eab84d275df9cb35bf2940a94b06db181aea2b35
Abdul Rahman Saleh <abdulrahmansaleh152@gmail.com>
2021-04-17 16:29:54 +0700

    update app bar halaman movie_list

--- lib/pages/movie_list.dart
+++ lib/pages/movie_list.dart
@@ -34,32 +34,53 @@ class _MovieListState extends State<MovieList> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Popular Movies"),
-      ),
-      body: ListView.builder(
-        itemCount: (this.moviesCount == null) ? 0 : this.moviesCount,
-        itemBuilder: (context, int position) {
-          return Card(
-            color: Colors.white,
-            elevation: 2.0,
-            child: ListTile(
-              leading: CircleAvatar(
-                radius: 27,
-                backgroundImage: NetworkImage(
-                    'https://image.tmdb.org/t/p/original' +
-                        movies[position].posterPath.toString()),
-              ),
-              title: Text(movies[position].title),
-              subtitle:
-                  Text('Rating = ' + movies[position].voteAverage.toString()),
-              onTap: () {
-                MaterialPageRoute route = MaterialPageRoute(
-                    builder: (_) => MovieDetail(movies[position]));
-                Navigator.push(context, route);
-              },
+        title: Row(
+          children: [
+            Text("NSC ",
+                style: TextStyle(color: Colors.orangeAccent, fontSize: 25)),
+            Text(
+              "Movie",
             ),
-          );
-        },
+          ],
+        ),
+        backgroundColor: Colors.black87,
+        actions: [
+          IconButton(
+            icon: Icon(
+              Icons.short_text,
+              color: Colors.white,
+              size: 33,
+            ),
+          ),
+        ],
+      ),
+      body: Container(
+        color: Colors.black87,
+        child: ListView.builder(
+          itemCount: (this.moviesCount == null) ? 0 : this.moviesCount,
+          itemBuilder: (context, int position) {
+            return Card(
+              color: Colors.white,
+              elevation: 2.0,
+              child: ListTile(
+                leading: CircleAvatar(
+                  radius: 27,
+                  backgroundImage: NetworkImage(
+                      'https://image.tmdb.org/t/p/original' +
+                          movies[position].posterPath.toString()),
+                ),
+                title: Text(movies[position].title),
+                subtitle:
+                    Text('Rating = ' + movies[position].voteAverage.toString()),
+                onTap: () {
+                  MaterialPageRoute route = MaterialPageRoute(
+                      builder: (_) => MovieDetail(movies[position]));
+                  Navigator.push(context, route);
+                },
+              ),
+            );
+          },
+        ),
       ),
     );
   }
