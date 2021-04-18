b94cd7d81f7159f224451ea099f6c9a94a8287ad
Abdul Rahman Saleh <abdulrahmansaleh152@gmail.com>
2021-04-18 15:14:24 +0700

    update app bar dan poster movie pada halaman movie_detail

--- lib/pages/movie_detail.dart
+++ lib/pages/movie_detail.dart
@@ -17,25 +17,77 @@ class MovieDetail extends StatelessWidget {
           'https://images.freeimages.com/images/large-previews/5eb/movie-clapboard-1184339.jpg';
     }
     double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(movie.title),
-      ),
-      body: SingleChildScrollView(
-        child: Center(
-          child: Column(
-            children: [
-              Container(
-                padding: EdgeInsets.all(16),
-                height: height / 1.5,
-                child: Image.network(path),
-              ),
-              Container(
-                child: Text(movie.overview),
-                padding: EdgeInsets.only(left: 16, right: 16),
-              )
-            ],
+        title: Text(
+          movie.title,
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
           ),
+        ),
+        backgroundColor: Color(0xFF151C26),
+      ),
+      backgroundColor: Color(0xFF151C26),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: height / 1.5,
+              width: width,
+              // child: Image.network(path),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: NetworkImage(path),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                      begin: Alignment.bottomCenter,
+                      end: Alignment.topCenter,
+                      colors: [
+                        Color(0xFF151C26).withOpacity(1.0),
+                        Color(0xFF151C26).withOpacity(0.0)
+                      ]),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.only(left: 15, bottom: 20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        movie.title,
+                        style: TextStyle(
+                          color: Color(0xFFfafafa),
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Container(
+                        child: Text(
+                          'Rating ' + movie.voteAverage.toString(),
+                          style: TextStyle(
+                            color: Colors.yellow,
+                            fontSize: 22,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              child: Text(movie.overview),
+              padding: EdgeInsets.only(left: 16, right: 16),
+            )
+          ],
         ),
       ),
     );
